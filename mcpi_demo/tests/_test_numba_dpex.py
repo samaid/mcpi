0630036a6d442b1c25b05b84846e28414998e7b0
samaid <sergey.maidanov@intel.com>
2023-04-27 09:05:02 -0500

    Update _test_numba_dpex.py
    Disable numba_dpex tests

--- mcpi_demo/tests/_test_numba_dpex.py
+++ mcpi_demo/tests/_test_numba_dpex.py
@@ -5,7 +5,7 @@ import dpnp as np
 from mcpi_demo.impl.impl_numba_dpex import monte_carlo_pi_batch
 
 
-def test_numpy():
+def test_numba_dpex():
     batch_size = 1000
     np.random.seed(7777777)
     pi = monte_carlo_pi_batch(batch_size)
