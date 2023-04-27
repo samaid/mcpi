2af14c2fbe251144146837d5a10a5df0fad4ac97
samaid <sergey.maidanov@intel.com>
2023-04-27 15:46:09 -0500

    Test improvements

--- mcpi_demo/tests/_test_numba_dpex.py
+++ mcpi_demo/tests/_test_numba_dpex.py
@@ -6,7 +6,7 @@ from mcpi_demo.impl.impl_numba_dpex import monte_carlo_pi_batch
 
 
 def test_numba_dpex():
-    batch_size = 1000
+    batch_size = 100000
     np.random.seed(7777777)
     pi = monte_carlo_pi_batch(batch_size)
     assert fabs(pi - 3.14) <= 0.1
--- mcpi_demo/tests/test_dpnp.py
+++ mcpi_demo/tests/test_dpnp.py
@@ -6,7 +6,7 @@ from mcpi_demo.impl.impl_dpnp import monte_carlo_pi_batch
 
 
 def test_dpnp():
-    batch_size = 1000
+    batch_size = 100000
     np.random.seed(7777777)
     pi = monte_carlo_pi_batch(batch_size)
     assert fabs(pi - 3.14) <= 0.1
